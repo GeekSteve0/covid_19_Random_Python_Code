1dbfee5a24eaa998ad13ebd160dd0f0892bc35da
techysteve <wanguistephen5@gmail.com>
2023-11-10 16:41:22 +0300

    A search implemented in python array

--- Search_array.py
+++ Search_array.py
@@ -4,7 +4,7 @@ searchable = array('i', [8,6,7,4,1,22])
 search = int(input("Enter a random number to search in our array: "))
 
 for element in searchable:
-    if searchable==element:
+    if search==element:
         print(f"{search} is in the array")
         break
 
